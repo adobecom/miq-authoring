918a6d2a0f8f2f23a0cd405d564415ddeec26504
Brad Johnson <fullcolorcoder@gmail.com>
2024-04-01 21:04:49 -0500

    Resolves MWPW-145588and MWPW-145589, updating paths and existence checks to improve experience.

--- src/components/common/sidebar/Sidebar.jsx
+++ src/components/common/sidebar/Sidebar.jsx
@@ -13,8 +13,8 @@ const Sidebar = ({ addQuestion, onImportData, exportData }) => {
     return (
         <aside>
             <div className="heading">
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 30 26" width="36px" xmlSpace="preserve" aria-hidden="true"> <path fill="#FA0F00" d="M19 0h11v26zM11.1 0H0v26zM15 9.6L22.1 26h-4.6l-2.1-5.2h-5.2z"></path> </svg>
-                <h1 className="spectrum-Heading spectrum-Heading--sizeXS">Quiz Authoring Tool</h1>
+                <a href="/miq-authoring"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 30 26" width="36px" xmlSpace="preserve" aria-hidden="true"> <path fill="#FA0F00" d="M19 0h11v26zM11.1 0H0v26zM15 9.6L22.1 26h-4.6l-2.1-5.2h-5.2z"></path> </svg>
+                <h1 className="spectrum-Heading spectrum-Heading--sizeXS">Quiz Authoring Tool</h1></a>
             </div>
             <sp-accordion>
                 <sp-accordion-item label="Build New Quiz">
